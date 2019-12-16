ab2942c05b6f34828006467489ae0e7a46e687a7
Juliana <juliana@itsencrypted.com>
2019-12-16 09:07:19 -0300

    Education Event - description & one image

--- apps/site/src/data/events.js
+++ apps/site/src/data/events.js
@@ -1,15 +1,15 @@
 export default [
   {
     alias: 'event-1',
-    title: 'Education Event 12345 ',
-    subtitle: 'A deep dive to Ethereum.',
+    title: 'Ethereum Developer Training - Day 2 ',
+    subtitle: 'Solidity Fundamentals',
     date: 'December 28, 2019',
     price: '35',
     token: 'DAI',
     content:
-      'Nam nulla mauris, dictum sit amet semper a, egestas vehicula nulla. Pellentesque tortor magna, varius in augue ac, hendrerit rhoncus metus. Cras feugiat velit in eros imperdiet fringilla. Nulla bibendum nulla eu porttitor pretium.',
+      'Solidity is a programming language for writing Smart Contracts. In this session, you will learn how to build a simple contract based application using Solidity. Students will learn the syntax and some programming basics: types, variables, functions, and conditionals. The course is designed for beginners willing to learn about the basics of blockchain and smart contracts.  You will then learn all about the Solidity ecosystem, including syntax and advanced concepts such as source mappings, then apply them to build  your very first contract.',
     image:
-      'https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE3MzYxfQ&auto=format&fit=crop&w=1950&q=80',
+      'https://imgur.com/gallery/6sQRw3E',
     imageBackground:
       'https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE3MzYxfQ&auto=format&fit=crop&w=1950&q=80',
     images: [
