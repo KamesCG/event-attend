53c12fb29f4d279b9bacf8c4b40435c5e680d170
Kames <info@kamescg.com>
2019-12-17 01:00:16 -0300

    Updates

---
+++ precache-manifest.cf72a9e281a90df4e8cb8a6ec4ebfb25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce48cafc7875b61f1abd66be0bcadd64",
+    "url": "/event-attend/index.html"
+  },
+  {
+    "revision": "15e3ba66cc0c34b7f6a2",
+    "url": "/event-attend/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "3fc8d6eb94dc96c32547",
+    "url": "/event-attend/static/css/main.ffd9d702.chunk.css"
+  },
+  {
+    "revision": "15e3ba66cc0c34b7f6a2",
+    "url": "/event-attend/static/js/2.f698638f.chunk.js"
+  },
+  {
+    "revision": "3fc8d6eb94dc96c32547",
+    "url": "/event-attend/static/js/main.3dd9f04d.chunk.js"
+  },
+  {
+    "revision": "f450b535cd5740c5c784",
+    "url": "/event-attend/static/js/runtime~main.551be7d3.js"
+  }
+]);
